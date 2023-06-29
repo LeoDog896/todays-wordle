60f1683174e12fd1a87061db488328d3a0b93f54
Tristan F <LeoDog896@hotmail.com>
2023-06-29 13:00:43 -0400

    feat: schema validation, README generation

---
+++ README.md
@@ -0,0 +1,30 @@
+# today's wordle generator
+
+uses the ny times API: `https://www.nytimes.com/svc/wordle/v2/YYYY-MM-DD.json`
+
+## yesterday
+
+<details>
+    <summary>click to see yesterday's wordle</summary>
+
+    tract
+
+</details>
+
+## today
+
+<details>
+    <summary>click to see today's wordle</summary>
+
+    diner
+
+</details>
+
+## tomorrow
+
+<details>
+    <summary>click to see tomorrow's wordle</summary>
+
+    straw
+
+</details>
--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
+import { z } from "https://deno.land/x/zod@v3.16.1/mod.ts";
+
+const wordleSchema = z.object({
+  id: z.number(),
+  solution: z.string(),
+  print_date: z.string(),
+  days_since_launch: z.number(),
+  editor: z.string(),
+});
+
 function formatDate(date: Date) {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
@@ -19,9 +29,41 @@ const [yesterday, today, tomorrow] = await Promise.all([
   getWordle(formatDate(new Date(Date.now() - 24 * 60 * 60 * 1000))),
   getWordle(formatDate(new Date())),
   getWordle(formatDate(new Date(Date.now() + 24 * 60 * 60 * 1000))),
-]);
+]).then((wordles) => wordles.map((wordle) => wordleSchema.parse(wordle)));
 
 await Deno.writeTextFile(
   "data.json",
   JSON.stringify({ yesterday, today, tomorrow }),
+);
+
+await Deno.writeTextFile(
+  "README.md",
+  `# today's wordle generator
+
+uses the ny times API: \`https://www.nytimes.com/svc/wordle/v2/YYYY-MM-DD.json\`
+
+## yesterday
+
+<details>
+    <summary>click to see yesterday's wordle</summary>
+
+    ${yesterday.solution}
+</details>
+
+## today
+
+<details>
+    <summary>click to see today's wordle</summary>
+
+    ${today.solution}
+</details>
+
+## tomorrow
+
+<details>
+    <summary>click to see tomorrow's wordle</summary>
+
+    ${tomorrow.solution}
+</details>
+`,
 );
